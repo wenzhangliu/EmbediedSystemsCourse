8f51c9ffe6769d22b1ae61e9e3711a3c5ac4d0ee
Wen-Zhang Liu <liu_wzh@foxmail.com>
2025-04-25 15:24:49 +0800

    ch7 tim

--- ch7-TIM/TIM_BreathLED/USER/main.c
+++ ch7-TIM/TIM_BreathLED/USER/main.c
@@ -2,62 +2,104 @@
 #include "stm32f10x_tim.h"
 #include "stm32f10x_gpio.h"
 
-u16 period_class = 10;
-u16 data_index = 0;
-u16 data[110]={1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 22, 25, 28};
-
-void tim2_init(u16 prescaler, u16 period)  // 时钟2配置程序
+void tim3_pwm_init(u16 prescaler, u16 period)  // 时钟2配置程序
 {
-	TIM_TimeBaseInitTypeDef tim_base_init;
-	TIM_OCInitTypeDef tim_oc_init;
-	NVIC_InitTypeDef nvic_init;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
 	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);  //TIM2时钟使能
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);	//使能定时器3时钟
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB  | RCC_APB2Periph_AFIO, ENABLE);  //使能GPIO外设和AFIO复用功能模块时钟
 	
-	//设定定时器定时
-	//计算公式：计数器频率=72MHz / prescaler；
-	//定时时间 = period / 计数器频率 = prescaler * period / 72MHz
-	tim_base_init.TIM_Prescaler = prescaler - 1;  //预分频（时钟分频）：prescaler
-	tim_base_init.TIM_Period = period - 1;  //自动重装载寄存器周期的值（定时时间）累计period次产生一次中断；
-	tim_base_init.TIM_CounterMode = TIM_CounterMode_Up;  //计数方向：向上计数
-	TIM_TimeBaseInit(TIM2, &tim_base_init);
+	GPIO_PinRemapConfig(GPIO_PartialRemap_TIM3, ENABLE); //Timer3部分重映射  TIM3_CH2->PB5    
+ 
+   //设置该引脚为复用输出功能,输出TIM3 CH2的PWM脉冲波形	GPIOB.5
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5; //TIM_CH2
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  //复用推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIO
+ 
+   //初始化TIM3
+	TIM_TimeBaseStructure.TIM_Period = period; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值
+	TIM_TimeBaseStructure.TIM_Prescaler =prescaler; //设置用来作为TIMx时钟频率除数的预分频值 
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure); //根据TIM_TimeBaseInitStruct中指定的参数初始化TIMx的时间基数单位
 	
-	//设置TIM2输出通道为PWM1模式
-	tim_oc_init.TIM_OCMode = TIM_OCMode_PWM1;
-	tim_oc_init.TIM_OCPolarity = TIM_OCPolarity_Low;  //LED是给低电平点亮
-	tim_oc_init.TIM_OutputState = TIM_OutputState_Enable;  //输出状态使能
-	tim_oc_init.TIM_Pulse = 0;
-	TIM_OC2Init(TIM2, &tim_oc_init);
-	
-	//设置TIM2的更新中断
-	TIM_ClearFlag(TIM2, TIM_FLAG_Update);  //清除TIM2更新中断标志
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);  //使能TIM2更新中断
-	nvic_init.NVIC_IRQChannel = TIM2_IRQn;  //更新中断
-	nvic_init.NVIC_IRQChannelCmd = ENABLE;  //打开中断通道
-	nvic_init.NVIC_IRQChannelPreemptionPriority = 0; //抢占式优先级
-	nvic_init.NVIC_IRQChannelSubPriority = 0;  //响应式优先级
-	NVIC_Init(&nvic_init);
-	
-	TIM_Cmd(TIM2, ENABLE);  //定时器2工作使能
+	//初始化TIM3 Channel2 PWM模式	 
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2; //选择定时器模式:TIM脉冲宽度调制模式2
+ 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性高
+	TIM_OC2Init(TIM3, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM3 OC2
+
+	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);  //使能TIM3在CCR2上的预装载寄存器
+ 
+	TIM_Cmd(TIM3, ENABLE);  //使能TIM3
 }
 
-void TIM2_IRQHandler(void)  //中断服务程序
+void led_init(void)
 {
-	if(TIM_GetITStatus(TIM2, TIM_IT_Update) == SET){
-		if(!--period_class){
-			period_class = 10;  //每溢出10次就更改脉宽，保证每种PWM输出一段时间
-			if(++data_index >= 110){
-				data_index = 0;  //重新从起始数值设定脉宽
-			}
-			TIM_SetCompare2(TIM2, data[data_index]);  //设定脉宽比较值
+ 
+ GPIO_InitTypeDef  GPIO_InitStructure;
+ 	
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOE, ENABLE);	 //使能PB,PE端口时钟
+	
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;				 //LED0-->PB.5 端口配置
+ GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
+ GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
+ GPIO_Init(GPIOB, &GPIO_InitStructure);					 //根据设定参数初始化GPIOB.5
+ GPIO_SetBits(GPIOB,GPIO_Pin_5);						 //PB.5 输出高
+
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	    		 //LED1-->PE.5 端口配置, 推挽输出
+ GPIO_Init(GPIOE, &GPIO_InitStructure);	  				 //推挽输出 ，IO口速度为50MHz
+ GPIO_SetBits(GPIOE,GPIO_Pin_5); 						 //PE.5 输出高
+}
+
+u8 led_light(void)
+{
+	if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5) == 1){  //如果LED是熄灭状态，则点亮。
+		GPIO_ResetBits(GPIOB, GPIO_Pin_5);
+		return 1;}  //返回值为1表示LED由熄灭状态被点亮。
+	else{
+		return 0;}  //返回值为0表示LED已经为点亮状态。
+}
+
+u8 led_dark(void)
+{
+	if(GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_5) == 0){  //如果LED是点亮状态，则熄灭。
+		GPIO_SetBits(GPIOB, GPIO_Pin_5);
+		return 1;}  //返回值为1表示LED由点亮状态被成功熄灭。
+	else{
+		return 0;}  //返回值为0表示LED已经为熄灭状态。
+}
+
+void TIM3_IRQHandler(void)   //TIM3中断
+{
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) //检查指定的TIM中断发生与否:TIM 中断源 
+	{
+		if(!led_light()){  //小灯状态翻转一次
+			led_dark();
 		}
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);  //清除中断标志位
+		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);  //清除TIMx的中断待处理位:TIM 中断源
 	}
 }
 
 int main(void){
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	tim2_init(200, 1024);  //定时0.5秒。
-	while(1){}
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	led_init();
+	tim3_pwm_init(0, 899);  //不分频。PWM频率=72000000/900=80Khz
+	u16 pwm_value = 0;
+	u8 direction = 1;
+	while(1)
+	{	
+		for(u16 i=0; i< 20000; i++);
+		if(direction==1)pwm_value++;
+		else pwm_value--;
+		
+		if(pwm_value > 300)direction=0;
+		if(pwm_value == 0)direction=1;
+		
+		TIM_SetCompare2(TIM3, pwm_value);
+	}
 	return 0;
 }
